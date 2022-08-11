be74beb20eae1492cbb67503b19bfedaa17c7046
Weisu Yin <weisuyin96@gmail.com>
2022-08-11 20:50:43 +0000

    fix

--- .github/workflow_scripts/test_tabular_cloud.sh
+++ .github/workflow_scripts/test_tabular_cloud.sh
@@ -2,12 +2,10 @@
 
 set -ex
 
-ADDITIONAL_TEST_ARGS=$1
-
 source $(dirname "$0")/env_setup.sh
 
 setup_build_env
 install_cloud
 
 cd cloud/
-python3 -m pytest -n 2 --junitxml=results.xml "$ADDITIONAL_TEST_ARGS" tests/unittests/test_cloud_predictor.py
+python3 -m pytest --junitxml=results.xml tests/unittests/test_cloud_predictor.py
